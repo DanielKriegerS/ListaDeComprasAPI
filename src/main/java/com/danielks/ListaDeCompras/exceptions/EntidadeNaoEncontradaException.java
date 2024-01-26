a9361c40dd37a16acf4fbc866cd3d7c6594b1324
dschifel <danielschifelbain@gmail.com>
2024-01-26 17:41:06 -0300

    Changed: updated message name

--- src/main/java/com/danielks/ListaDeCompras/exceptions/EntidadeNaoEncontradaException.java
+++ src/main/java/com/danielks/ListaDeCompras/exceptions/EntidadeNaoEncontradaException.java
@@ -1,5 +1,5 @@
 package com.danielks.ListaDeCompras.exceptions;
 
 public class EntidadeNaoEncontradaException extends RuntimeException{
-    public EntidadeNaoEncontradaException (String message){super (message);}
+    public EntidadeNaoEncontradaException (String mensagem){super (mensagem);}
 }
