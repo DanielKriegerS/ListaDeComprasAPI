13037a884e4013a5a0107dc2e80a72eaf3fde69d
dschifel <danielschifelbain@gmail.com>
2024-01-26 15:27:44 -0300

    Created: Produto DTO

---
+++ src/main/java/com/danielks/ListaDeCompras/models/ProdutoDTO.java
@@ -0,0 +1,12 @@
+package com.danielks.ListaDeCompras.models;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ProdutoDTO(
+        @NotNull Long id,
+        @NotNull Long listaId,
+        @NotBlank(message = "O nome não pode estar em branco") String nome,
+        @NotBlank(message = "O valor não pode ser 0") double valor
+) {
+}
