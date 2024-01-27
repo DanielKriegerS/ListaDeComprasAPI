5cb86ee980fdb749abc140763592fd260171b173
dschifel <danielschifelbain@gmail.com>
2024-01-27 14:11:15 -0300

    New: implemented getAll method + implemented especific exception

--- src/main/java/com/danielks/ListaDeCompras/services/ProdutoService.java
+++ src/main/java/com/danielks/ListaDeCompras/services/ProdutoService.java
@@ -1,10 +1,14 @@
 package com.danielks.ListaDeCompras.services;
 
 import com.danielks.ListaDeCompras.entities.Produto;
+import com.danielks.ListaDeCompras.exceptions.ProdutoExceptions.ProdutoNaoEncontradoException;
 import com.danielks.ListaDeCompras.models.ProdutoDTO;
 import com.danielks.ListaDeCompras.repositories.ProdutosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -31,5 +35,13 @@ public class ProdutoService {
         );
     }
 
+    public Optional<ProdutoDTO> recuperarTodosProdutos(Long id){
+        Optional<Produto> produtoOptional = repositorio.findById(id);
+        if(produtoOptional.isPresent()){
+            return produtoOptional.map(this::converteEmDTO);
+        } else {
+            throw new ProdutoNaoEncontradoException(id);
+        }
+    }
 
 }
