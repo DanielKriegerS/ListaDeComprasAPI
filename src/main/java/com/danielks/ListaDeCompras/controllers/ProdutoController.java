33f20e4a4159cb8113c81546d32ce393c0ca7d95
dschifel <danielschifelbain@gmail.com>
2024-01-28 12:27:24 -0300

    New: implemented post/create mapping

--- src/main/java/com/danielks/ListaDeCompras/controllers/ProdutoController.java
+++ src/main/java/com/danielks/ListaDeCompras/controllers/ProdutoController.java
@@ -5,9 +5,7 @@ import com.danielks.ListaDeCompras.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,10 @@ public class ProdutoController {
         return new ResponseEntity<>(produtos, HttpStatus.OK);
     }
 
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> criarProduto(@RequestBody ProdutoDTO produtoDTO) {
+        ProdutoDTO produtoCriado = service.criarProduto(produtoDTO);
+        return new ResponseEntity<>(produtoCriado, HttpStatus.CREATED);
+    }
 
 }
