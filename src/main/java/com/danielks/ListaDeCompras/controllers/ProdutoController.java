fa7ad24302bcb166b9f91b68a3d3217c93b3d83d
dschifel <danielschifelbain@gmail.com>
2024-01-28 12:22:53 -0300

    New: implemented getall mapping

--- src/main/java/com/danielks/ListaDeCompras/controllers/ProdutoController.java
+++ src/main/java/com/danielks/ListaDeCompras/controllers/ProdutoController.java
@@ -1,9 +1,15 @@
 package com.danielks.ListaDeCompras.controllers;
 
+import com.danielks.ListaDeCompras.models.ProdutoDTO;
 import com.danielks.ListaDeCompras.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -13,4 +19,12 @@ public class ProdutoController {
     private ProdutoService service;
 
     public ProdutoController(ProdutoService service)  {this.service = service;}
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoDTO>> recuperarTodosProdutos() {
+        List<ProdutoDTO> produtos = service.recuperarTodosProdutos();
+        return new ResponseEntity<>(produtos, HttpStatus.OK);
+    }
+
+
 }
